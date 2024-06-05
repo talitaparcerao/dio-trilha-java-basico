1a3ab57e5f12645b87cf1fbfd22ec50338080575
Talita Parcerão <talitasparcerao@gmail.com>
2024-06-05 00:41:35 -0300

    minha conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+		System.out.println("Por favor, Digite seu nome: ");
+		String nomeCliente = sc.nextLine();
+		System.out.println("Digite a sua agência");
+	    int agencia = sc.nextInt();
+        System.out.println("Digite o número da sua conta: ");
+	    int numero = sc.nextInt();
+        System.out.println("Digite o número da sua conta: ");
+	    double saldo = sc.nextDouble();
+
+        System.out.printf("Olá %s obrigado por criar uma conta em nosso banco sua agência é %d conta %d e seu saldo R$%f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+       
+        sc.close();
     }
 }
